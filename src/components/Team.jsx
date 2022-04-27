e742e93e0e2a97aa229e874142d5ba512b1cc069
Elvis Silva <elviscarlossouza@gmail.com>
2022-04-27 18:52:20 -0300

    update project

---
+++ src/components/Team.jsx
@@ -0,0 +1,30 @@
+export const Team = (props) => {
+  return (
+    <div id='team' className='text-center'>
+      <div className='container'>
+        <div className='col-md-8 col-md-offset-2 section-title'>
+          <h2>A Profissional</h2>
+          <p>
+            A Jessica é uma excelente profissional e vai cuidar do seu pet com todo amor e dedicação.
+          </p>
+        </div>
+        <div id='row'>
+          {props.data
+            ? props.data.map((d, i) => (
+                <div key={`${d.name}-${i}`} className='col-md-0 col-sm-0 team'>
+                  <div className='thumbnail'>
+                    {' '}
+                    <img src={d.img} alt='...' className='team-img' />
+                    <div className='caption'>
+                      <h4>{d.name}</h4>
+                      <p>{d.job}</p>
+                    </div>
+                  </div>
+                </div>
+              ))
+            : 'loading'}
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ src/components/services.jsx
@@ -0,0 +1,28 @@
+export const Services = (props) => {
+  return (
+    <div id='services' className='text-center'>
+      <div className='container'>
+        <div className='section-title'>
+          <h2>Nossos Serviços</h2>
+          <p>
+            Estes são todos os serviços a disposição do seu pet.
+          </p>
+        </div>
+        <div className='row'>
+          {props.data
+            ? props.data.map((d, i) => (
+                <div key={`${d.name}-${i}`} className='col-md-4'>
+                  {' '}
+                  <i className={d.icon}></i>
+                  <div className='service-desc'>
+                    <h3>{d.name}</h3>
+                    <p>{d.text}</p>
+                  </div>
+                </div>
+              ))
+            : 'loading'}
+        </div>
+      </div>
+    </div>
+  )
+}
